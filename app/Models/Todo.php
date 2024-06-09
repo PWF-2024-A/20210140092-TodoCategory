baa4c4c1b796e0918f6909b3208d935e02d4eaef
Bagas Saras <sarasbagas63@gmail.com>
2024-06-09 16:08:49 +0700

    api : auth, read, search data

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'title',
-        'user_id',
-        'category_id',
-        'is_complete',
+    protected $casts = [
+        // 'title',
+        // 'user_id',
+        // 'category_id',
+        'is_complete' => 'boolean',
     ];
     public function user()
     {
